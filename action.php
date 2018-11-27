3383bb38ad845f3af4804b062d03381186d61f4c
vashishtha <vashishtha.prime@gmail.com>
2018-11-27 22:21:54 +0530

    Initial commit

---
+++ action.php
@@ -0,0 +1,29 @@
+<?php  
+ //action.php  
+ include 'crud.php';  
+ $object = new Crud();  
+ if(isset($_POST["action"]))  
+ {  
+      if($_POST["action"] == "Load")  
+      {  
+           echo $object->get_data_in_table("SELECT * FROM users ORDER BY id DESC");  
+      }  
+      if($_POST["action"] == "Insert")  
+      {  
+           $first_name = mysqli_real_escape_string($object->connect, $_POST["first_name"]);  
+           $last_name = mysqli_real_escape_string($object->connect, $_POST["last_name"]);  
+           $image = $object->upload_file($_FILES["user_image"]);  
+           $query = "  
+           INSERT INTO users  
+           (name, user_name, password)   
+           VALUES ('".$first_name."', '".$last_name."', '".$image."')  
+           ";  
+           $object->execute_query($query);  
+           echo 'Data Inserted';  
+      }
+      if($_POST["action"] == "delete")  
+      {  
+           echo 'DATA DELETE HERE !!';  
+      } 
+ }  
+ ?>  
